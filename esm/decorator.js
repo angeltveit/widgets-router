ca1ba82a9991b624bc5d4bc407409dd99d07df87
angeltveit <=>
2018-10-12 18:09:39 +0200

    removed console.log

--- esm/decorator.js
+++ esm/decorator.js
@@ -3,7 +3,6 @@ const routes = new Set()
 export function Route(route, element) {
   return function run(self) {
     routes.add({route, self})
-    console.log('Route decorator ran', self)
   }
 }
 
